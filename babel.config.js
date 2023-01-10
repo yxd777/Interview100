b370829c6db6cbc6aa7317f1737e284a6926d8fb
yxd777 <792164257@qq.com>
2023-01-10 22:58:22 +0800

    项目初始化

---
+++ babel.config.js
@@ -0,0 +1,13 @@
+/*
+ * @Author: yxd
+ * @Date: 2023-01-10 20:44:02
+ * @LastEditors: yxd777 792164257@qq.com
+ * @LastEditTime: 2023-01-10 20:44:15
+ * @Description: 
+ */
+module.exports = {
+  presets: [
+    ['@babel/preset-env', {targets: {node: 'current'}}],
+    '@babel/preset-typescript',
+  ],
+};
